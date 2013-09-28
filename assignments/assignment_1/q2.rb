4d7a0f7800b537da704f34c1b22eb13efe8c1226
Nguyen Nhat Quang <nhatquang238@gmail.com>
2013-09-28 18:16:07 +0800

    answer for q2

---
+++ assignments/assignment_1/q2.rb
@@ -0,0 +1,10 @@
+# all case - duplicated letter - duplicated number - duplicated both number and letter
+a = 52*52*10*10*28*5! - 52*1*10*9*28*5!/2! - 52*51*10*1*28*5!/2! - 52*1*10*1*28*5!/(2!*2!)
+
+# bi AB or BA
+bi = 4!*4!*2
+# bii _ R1 _ R2 _ R3 _ R4 _
+bii = 4!*5P4
+
+ci = 10P4*20P4*2
+cii = 10P4*20P5 + 10P5*20P4
