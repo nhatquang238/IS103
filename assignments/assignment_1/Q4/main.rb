cd2da261e850c299c6821a74540c775d722db6b0
Pham Minh Khoa <khoa.zany@gmail.com>
2013-09-30 12:08:51 +0700

    Changed.Testing

--- assignments/assignment_1/Q4/main.rb
+++ assignments/assignment_1/Q4/main.rb
@@ -1,7 +1,6 @@
 # Project Phase 1, Q4
-# You may modify this file for testing purposes,
+# You may modify this file for testing purposes, 
 # but your final q4.rb must be able to run with the original main.rb.
-startTime = Time.now
 
 load "utility.rb"
 load "q4.rb"
@@ -15,21 +14,10 @@ read_file("CSV files/unsorted_restaurant_names1_v2.csv").each{ |line|
 }
 
 # perform sorting
-qsort(names, 0, names.length-1)
-
-# array = [523,1,13,63,214,6134,632]
-# p "length: " + array.length.to_s
-# qsort(array, 0, array.length - 1)
-# p partition(array, 0, array.length - 1)
-
-# for i in 0..array.length - 1
-# 	p array[i]
-# end
+qsort(names, 0, names.length-1) 
 
 # print out the sorted names
 for i in 0..names.length-1
   p i.to_s + " : " + names[i]
 end
 
-execTime = Time.now - startTime
-puts "Execution time #{execTime} seconds."
